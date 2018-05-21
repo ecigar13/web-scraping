c0b4fd622fb3d5fc2d963b4381f2624949a4e527
Khoa T. Nguyen <khoanguyen0791@gmail.com>
2018-05-21 15:28:50 -0500

    Completed the scraper. There might be a few other things to implement if I have time.

--- truyenyyScraper.py
+++ truyenyyScraper.py
@@ -4,6 +4,20 @@ import requests
 from requests.exceptions import HTTPError
 import time
 import os
+import random
+
+"""
+This proram access many stories in truyenyy.com and write to HTML file.
+
+Implemented parts:
+- Dealing with server errors
+- Added user agent
+- Randomized access interval (to avoid anti-scraping)
+Possible improvements:
+- More spoofing of user agent
+- Call flush to disk if can't overcome server errors
+- Add referer header 
+"""
 
 
 class TruyenyyScraper(object):
@@ -18,16 +32,27 @@ class TruyenyyScraper(object):
         self.outputFile = outputFile
 
     def openFile(self):
-        self.out = open(self.outputFile, "w+")
+        self.out = open(self.outputFile, "a+")
         self.writeHeadHtml()
 
     def writeHeadHtml(self):
-        self.out.write("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">")
+        """Write the beginning part of HTML file so the written file is not garbled when viewed in browser"""
+        self.out.write("""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+            <html>
+            <head>
+                <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+                <title></title>
+                <meta name="generator" content="LibreOffice 5.1.6.2 (Linux)"/>
+                <meta name="created" content="00:00:00"/>
+                <meta name="changed" content="00:00:00"/>
+            </head>
+            <body lang="en-US" dir="ltr">""")
 
     def writeToFile(self):
+        """Flush the buffer to OS then to disk when buffer reaches a certain size in case of a crash """
         for sections in self.chapters:
             #encoded = sections.encode('UTF-8')
-            
+
             self.out.write(str(sections))
         self.out.flush()
         os.fsync(self.out.fileno())
@@ -36,29 +61,53 @@ class TruyenyyScraper(object):
         self.chapterNum = 0
 
     def closeFile(self):
+        """ Close the tags of the HTML file so I don't have to manually use Libre office to fix it """
+        self.out.write("</body></html>")
+        self.out.flush()
+        os.fsync(self.out.fileno())
         self.out.close()
 
     def extractTitle(self, soup):
+        """ Extract the chapter title of most truyenyy pages."""
         return soup.find("h1", class_="chap-title sans-serif-font")
 
     def extractContent(self, soup):
+        """ Extract the main content of most truyenyy pages."""
         return soup.find("div", id="inner_chap_content_1")
 
     def setChapterUrl(self, page):
+        """ Set the current chapter URL, can be used to continue scraping if the last scraper crashed."""
         newUrl = self.storyUrl + str(page) + ".html"
         return newUrl
 
     # Return requests.model.Response
     def getResponseObject(self, url):
-        try:
-            r = requests.get(url)
-            r.raise_for_status()
-        except HTTPError:
-            print("Could not download page.")
-            self.closeFile()
-            exit(0)
-        else:
-            return r
+        """ Access the url and attempt to deal with different connection errors."""
+        while True:
+            try:
+                r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+                                 )
+                r.raise_for_status()
+            except HTTPError as errh:
+                print("Http Error:", errh)
+                print("It is likely that the program has reached the end of the story and is not an error.")
+                self.closeFile()
+                exit(0)
+            except requests.exceptions.ConnectionError as errc:
+                print("Error Connecting:", errc)
+                print("Retrying in a few seconds.")
+                time.sleep(5)
+                continue
+            except requests.exceptions.Timeout as errt:
+                print("Timeout Error:", errt)
+                print("Retrying in a few seconds.")
+                time.sleep(5)
+                continue
+            except requests.exceptions.RequestException as err:
+                print("OOps: Something Else", err)
+                self.writeToFile()
+            else:
+                return r
 
     # parser can be html.parser, lxml or other parsers
     def getSoup(self, requestsObject, parser):
@@ -71,21 +120,21 @@ class TruyenyyScraper(object):
         self.currentChapter += 1
 
     def cleanUp(self):
+        """Call this function every once in a while or after Ctrl+C to flush content to disk"""
         self.writeToFile()
         self.closeFile()
 
-def main():
+
+def main(storyName, saveFileName):
     try:
-        scraper = TruyenyyScraper("quan-than", "quan-than.html")
+        scraper = TruyenyyScraper(str(storyName), str(saveFileName))
         scraper.openFile()
         url = scraper.setChapterUrl(1)
         response = scraper.getResponseObject(url)
-        
-        count = 0
-        while(count < 20):
-        #while(response.status_code <= 399):
+
+        while(response.status_code < 400):
             print("Next chapter: " + str(scraper.currentChapter))
-            time.sleep(0.25)
+            time.sleep(random.uniform(0, 3))
             soup = scraper.getSoup(response, "lxml")
             scraper.addNextChapter(soup)
             scraper.chapterNum = scraper.chapterNum+1
@@ -94,9 +143,12 @@ def main():
 
             url = scraper.setChapterUrl(scraper.currentChapter)
             response = scraper.getResponseObject(url)
-            count+=1
+
     except KeyboardInterrupt:
         scraper.cleanUp()
 
+    print("Done scraping. File saved in : " + str(saveFileName))
+
+
 if __name__ == "__main__":
-    main()
+    main("quan-than", "quan-than.html")
